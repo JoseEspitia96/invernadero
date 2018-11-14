62e580269d9c19109c31367c87017d52406a8b22
espitiafaith <43999135+espitiafaith@users.noreply.github.com>
2018-11-14 18:42:17 -0500

    volver anterior

--- web/index.php
+++ web/index.php
@@ -26,15 +26,12 @@ $app->get('/fecha', function() use($app){
   return date('Y-m-d H:i:s');
 });
 
-$app->get('/guardar/{temperatura_ambiente}/{humedad_ambiente}/{humedad_suelo}/{luz}', 
-	function($temperatura_ambiente, $humedad_ambiente, $humedad_suelo, $luz) use($app){
+$app->get('/guardar/{temperatura_ambiente}', 
+	function($temperatura_ambiente) use($app){
 	$dbconexion=pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d7668c6higkn8l user=dvtjsetxbqhets password=e805ee92c1736a560cb20ce9bd4f3f967fd85b6b4baa4c6ee2934bfece6430b0");
 	$registro=array (
 		"FECHA"=>date('Y-m-d H:i:s'),
-		"TEMPERATURA"=>$temperatura_ambiente,
-		"HUMEDADAIRE" =>$humedad_ambiente),
-		"HUMEDADSUELO" =>$humedad_suelo),
-		"ILUMINACION" =>$luz);
+		"TEMPERATURA"=>$temperatura_ambiente;
 	$resultado=pg_insert ($dbconexion,'PARAMETROS',$registro);
 	return date('Y-m-d H:i:s');
 
