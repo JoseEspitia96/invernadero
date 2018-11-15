ef429c76170152446e2438b480407fce8102a1b7
espitiafaith <43999135+espitiafaith@users.noreply.github.com>
2018-11-14 20:33:33 -0500

    4 variables papa

--- web/index.php
+++ web/index.php
@@ -20,13 +20,15 @@ $app->get('/fecha', function() use($app){
   $app['monolog']->addDebug('logging output.');
   return date('Y-m-d H:i:s');
 });
-$app->get('/guardar/{luz}/{humedad_suelo}', 
-	function($luz,$humedad_suelo) use($app){
+$app->get('/guardar/[temperatura}/{humedad_suelo}/{humedad_ambiente}/{luz}', 
+	function($temperatura,$humedad_suelo,$humedad_ambiente,$luz) use($app){
 	$dbconexion=pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d7668c6higkn8l user=dvtjsetxbqhets password=e805ee92c1736a560cb20ce9bd4f3f967fd85b6b4baa4c6ee2934bfece6430b0");
 	$registro=array (
 		"FECHA"=>date('Y-m-d H:i:s'),
-		"ILUMINACION"=>$luz,
-		"HUMEDADSUELO"=>$humedad_suelo);
+		"TEMPERATURA"=>$temperatura,
+		"HUMEDADSUELO"=>$humedad_suelo,
+		"HUMEDADAIRE"=>$humedad_ambiente,
+		"ILUMINACION"=>$luz);
 	$resultado=pg_insert ($dbconexion,'PARAMETROS',$registro);
 	return date('Y-m-d H:i:s');
 	});
