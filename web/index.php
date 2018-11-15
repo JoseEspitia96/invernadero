572d6d6a6e01accf2c8e3065bb0349ab352724be
espitiafaith <43999135+espitiafaith@users.noreply.github.com>
2018-11-14 19:59:35 -0500

    probando con una variable

--- web/index.php
+++ web/index.php
@@ -20,14 +20,11 @@ $app->get('/fecha', function() use($app){
   $app['monolog']->addDebug('logging output.');
   return date('Y-m-d H:i:s');
 });
-$app->get('/guardar/{temperatura_ambiente}/{humedad_suelo}/{humedad_ambiente}/{luz}', 
-	function($temperatura_ambiente, $humedad_suelo, $humedad_ambiente,$luz) use($app){
+$app->get('/guardar/{luz}', 
+	function($luz) use($app){
 	$dbconexion=pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d7668c6higkn8l user=dvtjsetxbqhets password=e805ee92c1736a560cb20ce9bd4f3f967fd85b6b4baa4c6ee2934bfece6430b0");
 	$registro=array (
 		"FECHA"=>date('Y-m-d H:i:s'),
-		"TEMPERATURA"=>$temperatura_ambiente),
-		"HUMEDADSUELO"=>$humedad_suelo),
-		"HUMEDADAIRE"=>$humedad_ambiente),
 		"ILUMINACION"=>$luz);
 	$resultado=pg_insert ($dbconexion,'PARAMETROS',$registro);
 	return date('Y-m-d H:i:s');
