4887c79cfd2d4a1909cc9483cdb1a564313eede8
espitiafaith <43999135+espitiafaith@users.noreply.github.com>
2018-11-14 19:21:50 -0500

    INTENTO DE ENVIO LEGAL

--- web/index.php
+++ web/index.php
@@ -20,12 +20,12 @@ $app->get('/fecha', function() use($app){
   $app['monolog']->addDebug('logging output.');
   return date('Y-m-d H:i:s');
 });
-$app->get('/guardar/{humedad}', 
-	function($humedad) use($app){
+$app->get('/guardar/{temperatura_ambiente}', 
+	function($temperatura_ambiente) use($app){
 	$dbconexion=pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d7668c6higkn8l user=dvtjsetxbqhets password=e805ee92c1736a560cb20ce9bd4f3f967fd85b6b4baa4c6ee2934bfece6430b0");
 	$registro=array (
 		"FECHA"=>date('Y-m-d H:i:s'),
-		"HUMEDADSUELO"=>$humedad);
+		"TEMPERATURA"=>$temperatura_ambiente);
 	$resultado=pg_insert ($dbconexion,'PARAMETROS',$registro);
 	return date('Y-m-d H:i:s');
 	});
