933255ea6d26aaa7b70826f60e41d19ca4ae6aca
espitiafaith <43999135+espitiafaith@users.noreply.github.com>
2018-11-14 19:16:20 -0500

    recuperacion

--- web/index.php
+++ web/index.php
@@ -1,22 +1,17 @@
 <?php
 date_default_timezone_set('America/Bogota');
 require('../vendor/autoload.php');
-
 $app = new Silex\Application();
 $app['debug'] = true;
-
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
-
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
-
 // Our web handlers
-
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
@@ -25,16 +20,13 @@ $app->get('/fecha', function() use($app){
   $app['monolog']->addDebug('logging output.');
   return date('Y-m-d H:i:s');
 });
-
-$app->get('/guardar/{temperatura_ambiente}', 
-	function($temperatura_ambiente) use($app){
+$app->get('/guardar/{humedad}', 
+	function($humedad) use($app){
 	$dbconexion=pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d7668c6higkn8l user=dvtjsetxbqhets password=e805ee92c1736a560cb20ce9bd4f3f967fd85b6b4baa4c6ee2934bfece6430b0");
 	$registro=array (
 		"FECHA"=>date('Y-m-d H:i:s'),
-		"TEMPERATURA"=>$temperatura_ambiente;
+		"HUMEDADSUELO"=>$humedad);
 	$resultado=pg_insert ($dbconexion,'PARAMETROS',$registro);
 	return date('Y-m-d H:i:s');
-
 	});
-
-$app->run();
+$app->run();  
