d5eaa71f5e26baa63c60a02a9d4ae9b719f5aa41
heyderpaez <heyderpaez@gmail.com>
2018-11-16 18:41:17 -0500

    llamado de librerias y correcciones en index.php

--- web/index.php
+++ web/index.php
@@ -1,6 +1,10 @@
 <?php
 date_default_timezone_set('America/Bogota');
 require('../vendor/autoload.php');
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 $app = new Silex\Application();
 $app['debug'] = true;
 // Register the monolog logging service
@@ -16,10 +20,7 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
-$app->get('/fecha', function() use($app){
-  $app['monolog']->addDebug('logging output.');
-  return date('Y-m-d H:i:s');
-});
+
 $app->get('/guardar/{temperatura}/{humedad_suelo}/{humedad_ambiente}/{luz}', 
 	function($temperatura,$humedad_suelo,$humedad_ambiente,$luz) use($app){
 	$dbconexion=pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d7668c6higkn8l user=dvtjsetxbqhets password=e805ee92c1736a560cb20ce9bd4f3f967fd85b6b4baa4c6ee2934bfece6430b0");
@@ -34,10 +35,9 @@ $app->get('/guardar/{temperatura}/{humedad_suelo}/{humedad_ambiente}/{luz}',
 	});
 
 $app->get('/getInvernaderoData/{numberOfRecords}', function($numberOfRecords) use($app){
-  $app['monolog']->addDebug('logging output.');
 
   $dbconn = pg_connect( "host=ec2-23-21-192-179.compute-1.amazonaws.com port=5432 dbname=d7668c6higkn8l user=dvtjsetxbqhets password=e805ee92c1736a560cb20ce9bd4f3f967fd85b6b4baa4c6ee2934bfece6430b0");
-	$consult_db = pg_query($dbconn, 'SELECT * FROM PARAMETROS ORDER BY "FECHA" DESC LIMIT ' . $numberOfRecords .'');
+  $consult_db = pg_query($dbconn, 'SELECT * FROM PARAMETROS ORDER BY "FECHA" DESC LIMIT ' . $numberOfRecords .'');
   
   $resultArray = array();
   while ($row = pg_fetch_array($consult_db, null, PGSQL_ASSOC)) {
